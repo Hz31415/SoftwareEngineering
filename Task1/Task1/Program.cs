a2582a964ecea71dadb198c031b957f25e4368a4
Hz31415 <56264718+Hz31415@users.noreply.github.com>
2021-09-16 16:37:37 +0400

    Add files via upload

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int Main(int n, int[] cars, int k)
+        {
+            Array.Sort(cars);
+
+            int length = cars[cars.Length - 1] - cars[0];
+            for (int i = 0; i < cars.Length - k; i++)
+            {
+                if (length > (cars[i + k - 1] - cars[i] + 1))
+                    length = cars[i + k - 1] - cars[i] + 1;
+            }
+
+            return length;
+        }
+    }
+}
